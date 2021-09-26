ec2141f107e185036430f7ccb3f7dd6c90957e24
Aleksey <aay92@yandex.ru>
2021-09-26 12:16:45 +0400

    add hide text

--- HelloWord/ViewController.swift
+++ HelloWord/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var helloWorldLable: UILabel!
+    
+    @IBOutlet var toglleButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        helloWorldLable.isHidden = true
+//        toglleButton.layer.cornerRadius = 30
+        
     }
-
+    @IBAction func makeButtonAction() {
+        if helloWorldLable.isHidden {
+            helloWorldLable.isHidden = false
+            toglleButton.setTitle("Текст показан", for: .normal)
+        } else {
+            helloWorldLable.isHidden = true
+            toglleButton.setTitle("Текст скрыт", for: .normal)
+        }
+    }
+    
 
 }
 
